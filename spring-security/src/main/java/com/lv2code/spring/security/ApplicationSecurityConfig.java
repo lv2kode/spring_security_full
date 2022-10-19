728b994dca60ca9919e16ac27cee6ab02859e149
lovetwocode <adeeb@thorsignia.in>
2022-10-19 21:58:04 +0530

    Basic Security Commit

---
+++ spring-security/src/main/java/com/lv2code/spring/security/ApplicationSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.lv2code.spring.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private final PasswordEncoder passwordEncoder;
+	
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		// when to use csrf protection?
+		// our recommendation is to use csrf protection for any request that could be processed
+		// by a browser by normal users. If you are only creating a service that is used by non-browser clients,
+		// you will likely want to disable CSRF protection.
+//		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//		.and()
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+		.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+		//Instead of these 4 below code snippet we can use directly method level security
+		//.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+		//.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+		//.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+		//.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+	}
+	
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails annaSmithUser = User.builder()
+				.username("annasmith")
+				.password(passwordEncoder.encode("password"))
+//				.roles(ApplicationUserRole.STUDENT.name()) // ROLE_STUDENT
+				.authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
+				.build();
+		
+		UserDetails lindaUser = User.builder()
+				.username("linda")
+				.password(passwordEncoder.encode("password123"))
+//				.roles(ApplicationUserRole.ADMIN.name()) // ROLE_ADMIN
+				.authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
+				.build();
+		
+		UserDetails tomUser = User.builder()
+				.username("tom")
+				.password(passwordEncoder.encode("password123"))
+//				.roles(ApplicationUserRole.ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+				.authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
+				.build();
+		
+		return new InMemoryUserDetailsManager(annaSmithUser, lindaUser, tomUser);
+	}
+}
