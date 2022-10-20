1bb56e88fe3eea3977346541c3f47ba60af84215
lovetwocode <adeeb@thorsignia.in>
2022-10-20 11:34:19 +0530

    Logout Custom Configuration

--- spring-security/src/main/java/com/lv2code/spring/security/ApplicationSecurityConfig.java
+++ spring-security/src/main/java/com/lv2code/spring/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -61,6 +62,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		.rememberMe() // defaults to 2 weeks
 			.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
 			.key("somethingverysecured")
+			
+		// Custom logout configuration
+		.and()
+		.logout()
+			.logoutUrl("/logout")
+			// CSRF is disabled because of that we should use the below line of code and it will be any http method
+			// if in case csrf is enabled then we should use post method only
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+			.clearAuthentication(true)
+			.invalidateHttpSession(true)
+			.deleteCookies("JSESSIONID", "remember-me")
+			.logoutSuccessUrl("/login")
 		;		
 	}
 	
